b7ff74fe1f05e4bce305a1b1e7ad98dcc88bee40
EStherMmutisya <starjacob994@gmail.com>
2024-03-18 08:38:55 +0300

    unfinished code along

--- lib/department.py
+++ lib/department.py
@@ -152,3 +152,16 @@ class Department:
         return [
             Employee.instance_from_db(row) for row in rows
         ]
+    def employees(self):
+        """Return list of employees associated with current department"""
+        from employee import Employee
+        sql = """
+            SELECT * FROM employees
+            WHERE department_id = ?
+        """
+        CURSOR.execute(sql, (self.id,),)
+
+        rows = CURSOR.fetchall()
+        return [
+            Employee.instance_from_db(row) for row in rows
+        ]
